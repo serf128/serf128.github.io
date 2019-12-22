e0c1d48ea0280ffc16f54f9e92cd34d127557d2e
serf128 <cifromobil@gmail.com>
2019-12-22 19:50:07 +0300

    fix video

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.article-video__iframe').forEach(el => {
-        if (document.documentElement.clientWidth >= 768) {
-            el.style.backgroundImage = `url('http://i.ytimg.com/vi/${el.id}/hqdefault.jpg')`;
-            el.addEventListener('click', () => {
-                el.innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${el.id}?autoplay=1" ></iframe>`
-            })
-        } else {
+        el.style.backgroundImage = `url('http://i.ytimg.com/vi/${el.id}/hqdefault.jpg')`;
+        el.addEventListener('click', () => {
             el.innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${el.id}?autoplay=1" ></iframe>`
-        }
+        })
     })
 });
